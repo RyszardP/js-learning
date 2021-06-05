324707a0deac77c885fd8fa773f6fe7c88257cda
rysard <Rysard_Poplavskij@epam.com>
2021-06-05 11:57:09 +0300

    refactor

--- module3/task3/tests/relations.test.js
+++ module3/task3/tests/relations.test.js
@@ -23,10 +23,17 @@ describe("Tests", () => {
             expect(testBoy.birthdayMonth).toBe('JUNE')
         });
 
+        it('check boy setbirthday method', () => {
+            testBoy = new Boy('JULY', 300);
+            testBoy.birthdayMonth = 'JUNE'
+            expect(testBoy.isSummerMonth()).toBe('JUNE')
+        });
+
         it('check boy birthday JULY month is summer month', () => {
             testBoy = new Boy('JULY', 300);
             expect(testBoy.isSummerMonth()).toBeTruthy;
         });
+
 
         it('check boy birthday AUGUST month is summer month', () => {
             testBoy = new Boy('AUGUST', 300);
@@ -45,11 +52,28 @@ describe("Tests", () => {
             expect(testBoy.getMood()).toBe("EXCELLENT");
         });
 
+
+        it('get mood function must  be EXCELLENT !', () => {
+            testBoy = new Boy('JUNE', 1100000000);
+            testBoy.isRich = () => true;
+            testBoy.isPrettyGirlFriend = () => true;
+            testBoy.isSummerMonth = () => true;
+            expect(testBoy.getMood()).toBe("EXCELLENT");
+        });
+
         it('check good mood should be NEUTRAL', () => {
             testBoy = new Boy('JUNE', 1100000000);
             testGirl = new Girl(true, true);
             testBoy.girlFriend = testGirl;
             expect(testBoy.getMood()).toBe('NEUTRAL');
+        });
+
+        it('check good  mood function must be NEUTRAL!', () => {
+            testBoy = new Boy('JUNE', 1100000000);
+            testBoy.isRich = () => true;
+            testBoy.isPrettyGirlFriend = () => false;
+            testBoy.isSummerMonth = () => true;
+            expect(testBoy.getMood()).toBe("NEUTRAL");
         });
 
         it('check good mood should be GOOD', () => {
@@ -59,6 +83,14 @@ describe("Tests", () => {
             expect(testBoy.getMood()).toBe('GOOD');
         });
 
+        it('check good mood function must be GOOD!', () => {
+            testBoy = new Boy('December', 500);
+            testBoy.isRich = () => true;
+            testBoy.isPrettyGirlFriend = () => true;
+            testBoy.isSummerMonth = () => false;
+            expect(testBoy.getMood()).toBe("GOOD");
+        });
+
         it('check good mood should be BAD', () => {
             testBoy = new Boy('DECEMBER', 500);
             testGirl = new Girl(undefined, undefined);
@@ -66,9 +98,25 @@ describe("Tests", () => {
             expect(testBoy.getMood()).toBe('BAD');
         });
 
+        it('check good mood function must be BAD!', () => {
+            testBoy = new Boy();
+            testBoy.isRich = () => false;
+            testBoy.isPrettyGirlFriend = () => false;
+            testBoy.isSummerMonth = () => false;
+            expect(testBoy.getMood()).toBe("BAD");
+        });
+
         it('check does spend amount of money', () => {
             testBoy = new Boy('JUNE', 300);
             let moneySpent = 300;
+            let result = (testBoy.wealth) - moneySpent;
+            testBoy.spendSomeMoney(300);
+            expect(testBoy.wealth).toBe(result);
+        });
+
+        it('check does spend more money than hew have', () => {
+            testBoy = new Boy('JUNE', 300);
+            let moneySpent = 500;
             let result = (testBoy.wealth) - moneySpent;
             testBoy.spendSomeMoney(300);
             expect(testBoy.wealth).toBe(result);
@@ -146,10 +194,28 @@ describe("Tests", () => {
             expect(testGirl.getMood()).toBe("EXCELLENT");
         });
 
+        it('girl mood must be EXCELLENT', () => {
+            testGirl = new Girl();
+            testGirl.isSlimFriendBecameFat = () => true;
+            testGirl.isPretty = () => true;
+            testGirl.isBoyfriendRich = () => true;
+            testGirl.isBoyFriendWillBuyNewShoes = () => true;
+            expect(testGirl.getMood()).toBe("EXCELLENT");
+        });
+
         it('pretty girl has boyfiend should have good mood ', () => {
             testBoy = new Boy('JUNE', 500);
             testGirl = new Girl(true, true);
             testGirl.boyFriend = testBoy;
+            expect(testGirl.getMood()).toBe("GOOD");
+        });
+
+        it('girl mood must be GOOD', () => {
+            testGirl = new Girl();
+            testGirl.isBoyFriendWillBuyNewShoes = () => false;
+            testGirl.isSlimFriendBecameFat = () => false;
+            testGirl.isPretty = () => true;
+            testGirl.isBoyfriendRich = () => true;
             expect(testGirl.getMood()).toBe("GOOD");
         });
 
@@ -161,21 +227,46 @@ describe("Tests", () => {
             expect(testGirl.getMood()).toBe("NEUTRAL");
         });
 
-        it('pretty girl should have bad mood ', () => {
+        it('girl mood must be NEUTRAL', () => {
+            testGirl = new Girl();
+            testGirl.isBoyFriendWillBuyNewShoes = () => false;
+            testGirl.isSlimFriendBecameFat = () => true;
+            testGirl.isPretty = () => false;
+            testGirl.isBoyfriendRich = () => false;
+            expect(testGirl.getMood()).toBe("NEUTRAL");
+        });
+
+        it('pretty girl should have BAD mood ', () => {
             testBoy = new Boy('DECEMBER', 500);
             testGirl = new Girl();
             testGirl.boyFriend = testBoy;
             expect(testGirl.getMood()).toBe("BAD");
         });
 
+        it('girl mood must be BAD', () => {
+            testGirl = new Girl();
+            testGirl.isBoyFriendWillBuyNewShoes = () => false;
+            testGirl.isSlimFriendBecameFat = () => false;
+            testGirl.isPretty = () => false;
+            testGirl.isBoyfriendRich = () => false;
+            expect(testGirl.getMood()).toBe("BAD");
+        });
+
         it('should spend amount of boyfriend money', () => {
-            testBoy = new Boy('JUNE', 1500);
+            testBoy = new Boy('JUNE', 100000000);
             testGirl = new Girl();
             testGirl.boyFriend = testBoy;
-            let testBoyMoneyBeforeSpending = testGirl.boyFriend.wealth;
-            testGirl.spendBoyFriendMoney(9000);
-            let testBoyMoneyAfterSpending = testGirl.boyFriend.wealth;
-            expect(testBoyMoneyAfterSpending).toBe(testBoyMoneyBeforeSpending)
+            testGirl.isBoyfriendRich = () => true;
+            testGirl.spendBoyFriendMoney(500);
+            expect(testGirl.spendBoyFriendMoney()).toBe(500)
         });
+
+        it('pretty girl should have bad mood', () => {
+            testGirl = new Girl();
+            testGirl.isSlimFriendGotAFewKilos = () => true;
+            testGirl.isPretty = () => true;
+            expect(testGirl.isSlimFriendBecameFat()).toBeTruthy();
+        });
+
     });
 });
